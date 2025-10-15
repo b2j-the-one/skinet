dd595674da506c3c2f4bea78c4059de8879e020a
b2jtheone <b2jtheone@gmail.com>
2025-10-15 23:04:23 +0200

    Fin de la section 9 : Building the user interface for the shop

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,27 +1,14 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { HeaderComponent } from "./layout/header/header.component";
-import { HttpClient } from '@angular/common/http';
-import { Product } from './shared/models/product';
-import { Pagination } from './shared/models/pagination';
+import { ShopComponent } from "./features/shop/shop.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  standalone: true,
+  imports: [HeaderComponent, ShopComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
-  private http = inject(HttpClient);
-
-  baseUrl = 'https://localhost:5001/api/';
+export class AppComponent {
   protected readonly title = signal('Koteka');
-  products: Product[] = [];
-
-  ngOnInit(): void {
-    this.http.get<Pagination<Product>>(this.baseUrl + 'products').subscribe({
-      next: response => this.products = response.data,
-      error: error => console.log(error),
-      complete: () => console.log('complete')
-    });
-  }
 }
